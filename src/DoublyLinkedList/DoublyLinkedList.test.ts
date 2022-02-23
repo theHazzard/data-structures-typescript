8471ace085b10f5f05d59604626f418a99ec1305
Enzo Luis Strongoli <thehazzard@gmail.com>
2022-02-23 18:19:30 -0300

    added push code with tests

--- src/DoublyLinkedList/DoublyLinkedList.test.ts
+++ src/DoublyLinkedList/DoublyLinkedList.test.ts
@@ -6,3 +6,31 @@ test("should create an empty Double Linked List", () => {
   expect(dll.tail).toBeNull();
   expect(dll.length).toBe(0);
 });
+
+test("should be able to push an item to the end using push", () => {
+  const dll = new DoublyLinkedList();
+  dll.push(15);
+  expect(dll.head?.value).toBe(15);
+  expect(dll.tail?.value).toBe(15);
+  expect(dll.length).toBe(1);
+
+  dll.push(16);
+  expect(dll.head?.value).toBe(15);
+  expect(dll.tail?.value).toBe(16);
+  expect(dll.length).toBe(2);
+  expect(dll.head?.next?.value).toBe(16);
+  expect(dll.head?.prev).toBeNull();
+  expect(dll.head?.next?.prev?.value).toBe(15);
+  expect(dll.head?.next?.next).toBeNull();
+
+  dll.push(17);
+  expect(dll.head?.value).toBe(15);
+  expect(dll.tail?.value).toBe(17);
+  expect(dll.length).toBe(3);
+  expect(dll.head?.prev).toBeNull();
+  expect(dll.head?.next?.value).toBe(16);
+  expect(dll.head?.next?.prev?.value).toBe(15);
+  expect(dll.head?.next?.next?.value).toBe(17);
+  expect(dll.head?.next?.next?.prev?.value).toBe(16);
+  expect(dll.head?.next?.next?.next).toBeNull();
+});
--- src/DoublyLinkedList/DoublyLinkedList.ts
+++ src/DoublyLinkedList/DoublyLinkedList.ts
@@ -6,7 +6,34 @@ class ListNode<T> {
 }
 
 export class DoublyLinkedList<T> {
-  head: ListNode<T> | null = null;
-  tail: ListNode<T> | null = null;
-  length: number = 0;
+  _head: ListNode<T> | null = null;
+  _tail: ListNode<T> | null = null;
+  _length: number = 0;
+
+  get length() {
+    return this._length;
+  }
+
+  get head() {
+    return this._head;
+  }
+
+  get tail() {
+    return this._tail;
+  }
+
+  push(value: T) {
+    const newNode = new ListNode(value);
+    if (this._length === 0) {
+      this._head = newNode;
+      this._tail = newNode;
+    } else {
+      const oldTail = this._tail!;
+      oldTail.next = newNode;
+      newNode.prev = this._tail;
+      this._tail = newNode;
+    }
+
+    this._length++;
+  }
 }
